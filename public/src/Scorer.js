3c446fcc4c7be0656fb89349652a394b8c058d4b
cesine <cesine@yahoo.com>
2012-04-14 11:28:51 -0400

    adding images and more instructions

--- public/src/Scorer.js
+++ public/src/Scorer.js
@@ -33,12 +33,30 @@ SCORES BY LINGUISTIC LEVEL AND SKILL
 
 */
 
+/*
+ * Adding and removing class on DOM elements in a safe manner
+ * incase there are mulitiple classes
+ */
+function hasClass(ele,cls) {
+  return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+function addClass(ele,cls) {
+  if (!this.hasClass(ele,cls)) ele.className += " "+cls;
+}
+function removeClass(ele,cls) {
+  if (hasClass(ele,cls)) {
+      var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)','g');
+      ele.className=ele.className.replace(reg,' ');
+  }
+}
+
 var scoreBAT = function(){
 	participant.score = {};
 	window.data = participant.data;
     if(!window.data){
         return;
     }
+    removeClass(document.getElementById("score_zone"),"hidden");
 	scorePhonology();
 	scoreMorphology();
 	scoreLexicon();
